f385ebfa83b81b1b65ffc31d3dd15f04bd10bcdd
Jackie <jackierhammer@gmail.com>
2022-09-02 23:19:53 -0600

    modified PORT for deploying to Heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const uuid = require('uuid-with-v6');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 let db = require('./db.json');
 
